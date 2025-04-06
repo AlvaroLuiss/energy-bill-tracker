286fdf82672af3f4e35d45f66663a0de2ba831de
alvaroluiss <alvaro.luiscosta@hotmail.com>
2025-04-06 16:27:17 -0300

    alvaro : #36 : update configurations

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  compiler: {
+    styledComponents: true
+  }
 };
 
 export default nextConfig;
