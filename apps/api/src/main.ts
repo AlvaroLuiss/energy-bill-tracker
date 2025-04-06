6ce18d6f15a253b4e992ab30daf38ccf9075176a
alvaroluiss <alvaro.luiscosta@hotmail.com>
2025-04-05 23:11:20 -0300

    alvaro : #23 : refactor add globla config to CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from '../src/infra/app.module'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/infra/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap()
