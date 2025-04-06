7cac9769bb628e298fa8cfdc8a3283450689290f
alvaroluiss <alvaro.luiscosta@hotmail.com>
2025-04-06 01:47:36 -0300

    alvaro : #25 : add get Client controller in http Module

--- apps/api/src/infra/database/http/http.module.ts
+++ apps/api/src/infra/database/http/http.module.ts
@@ -1,12 +1,14 @@
 import { UploadBillService } from "@/domain/lumi/application/services/bill/upload-bill.service";
+import { GetClientsService } from "@/domain/lumi/application/services/client/get-clients.service";
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "../database.module";
 import { UploadBillController } from "./controllers/bill/upload-bill.controller";
+import { GetClientsController } from "./controllers/client/get-clients.controller";
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [UploadBillController],
-  providers: [UploadBillService],
+  controllers: [UploadBillController, GetClientsController],
+  providers: [UploadBillService, GetClientsService],
   exports: [],
 })
 export class HttpModule {}
